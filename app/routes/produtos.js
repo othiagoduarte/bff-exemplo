f9f5a4ac75d7ba1e2b884a8c08ef8dfff45313a6
Thiago Duarte <thiago-duarte@outlook.com>
2017-08-15 00:33:30 -0300

    Refactor produtos

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -10,10 +10,28 @@ module.exports = function (app)
             return res.status(500).json(error.toString());
         }
     });
-
+    
     app.get("/produtos/:id", async (req, res) =>{
         try {
             res.jsonp(await ctrl.get(req, res));   
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json(error.toString());
+        }
+    });
+
+    app.post("/produtos", async (req, res) =>{
+        try {
+            res.jsonp(await ctrl.add(req, res));   
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json(error.toString());
+        }
+    });
+
+    app.put("/produtos", async (req, res) =>{
+        try {
+            res.jsonp(await ctrl.save(req, res));   
         } catch (error) {
             console.log(error);
             return res.status(500).json(error.toString());
