31e91431ebf403b4c700a2994691963038961cd6
Thiago Duarte <thiago-duarte@outlook.com>
2017-10-27 12:23:33 -0200

    Update dados.js

--- app/enuns/dados.js
+++ app/enuns/dados.js
@@ -1,6 +1,6 @@
 module.exports = function (app) 
 {
-    const CONST_URI_BASE = "https://api.alkord.com/"; 
+    const CONST_URI_BASE = "https://127.0.0.1/"; 
     const CONST_FINALIDADE = "1";  
     
     return {CONST_URI_BASE, CONST_FINALIDADE}
